749982c4a499253fdf64c79147875fd6c123a915
Chris Johns <chrisfotosmusic@gmail.com>
2021-05-01 23:38:24 -0400

    add arrow buttons

--- app.js
+++ app.js
@@ -1,42 +1,31 @@
 /* 
 TODO: 
-
-1. all the hard-coded values need to be made dynamic and passed to the 
-constructor. This should include the gutter width, the main slide's width, 
-and the regular slide's width. Those values should be applied to the elements
-in the constructor function. 
-    - decided not to set values in the constructor. Only made gutters dynamic. 
-    more trouble than it was worth, required a lot of changes 
-    to the current implementation. user can simply tell the 
-    slider what the values are. As it is, the user can change the widths to be whatever
-    they want in css, as long as they pass that value into the constructor.
-
 3. arrow buttons
-
 4. making the overflow visible. maybe just an SVG background.
-
-5. making it adjust on window resize. The logic that sets the offset in the constructor
-can be extracted into a method, and then that method can be passed to a 
-resize event listener.
 */
 
 class Sliderize {
   constructor(sliderDiv, gutter, itemWidth, mainSlideWidth){
     this.sliderDiv   = sliderDiv
-    this.sliderItems = Array.from(sliderDiv.children)   
+    this.sliderItems = Array.from(sliderDiv.getElementsByClassName('custom-product-slider__item'))   
     this.activeImgIndex = Math.floor(this.sliderItems.length / 2);
+    this.mainSlideWidth = mainSlideWidth
+
+    this.backButton = sliderDiv.getElementsByClassName('custom-slider__back')[0]
+    this.forwardButton = sliderDiv.getElementsByClassName('custom-slider__forward')[0]
 
     this.setGutter(gutter)
 
-    // slidelength is an item width + a gutter
     this.slideLength = itemWidth + gutter;
-
-    // incoming slidelength is a width + a gutter + the diff between a slide and a main slide
     this.incomingSlideLength = itemWidth + gutter + ((mainSlideWidth - itemWidth) / 2) 
 
-    this.setOffset()
+
     this.setActiveImg()
+    this.intializeButtons()
+    this.setOffset()
     this.applyOffset()
+    
+
     
     // set everything back to default state on window resize. If not, things get messed up. 
     window.addEventListener('resize', ()=>{
@@ -46,30 +35,54 @@ class Sliderize {
       this.activeImg.classList.remove('active-img')
       this.setActiveImg()
     })
+
+    console.log('main img rect', this.activeImg.getBoundingClientRect())
+
   }
 
+  intializeButtons(){
+    console.log('init btns')
+    this.backButton.addEventListener('click', ()=>this.goBackward())
+    this.forwardButton.addEventListener('click', ()=>this.goForward())
+
+    this.backButton.style.width = '50px'
+    this.forwardButton.style.width = '50px'
+    this.backButton.style.height = '50px'
+    this.forwardButton.style.height = '50px'
+    this.forwardButton.style.position = 'absolute'
+    this.backButton.style.position = 'absolute'
+    this.forwardButton.style.zIndex = '2'
+    this.backButton.style.zIndex = '2'
+  }
+
+
+  // Center elements
   setOffset(){
-    console.log('setting offset')
     this.offset =  
     // half the container's width
     parseInt(getComputedStyle(sliderDiv).getPropertyValue('width').split('p')[0]) / 2 
     // minus one slidelength * half the number of imgs 
     - (this.slideLength * Math.floor(this.sliderItems.length / 2 - 1)) 
     // minus an incoming slidelength
-    - this.incomingSlideLength
+    - this.incomingSlideLength;
+
+    //${this.activeImg.getBoundingClientRect().left - 5}
+
+    this.backButton.style.left = `${this.sliderDiv.getBoundingClientRect().width/2 - (this.mainSlideWidth/1.65)}px`
+    this.forwardButton.style.left = `${this.sliderDiv.getBoundingClientRect().width/2 + (this.mainSlideWidth/2.3)}px`
+    console.log(this.forwardButton.style.left )
   }
 
   applyOffset(){
-    Array.from(sliderDiv.children).forEach(element => {
+    this.sliderItems.forEach(element => {
       element.style.left = this.offset + 'px';
-      Array.from(sliderDiv.children)[this.activeImgIndex].style.left = this.offset + 100 + 'px'
+      this.sliderItems[this.activeImgIndex].style.left = this.offset + 100 + 'px'
     })
   }
 
   setActiveImg(){
     this.activeImg = Array.from(sliderDiv.children)[this.activeImgIndex]
     this.activeImg.classList.add('active-img')
-    console.log(this.activeImg)
   }
 
   setGutter(gutter){ 
@@ -77,6 +90,7 @@ class Sliderize {
   }
 
   goForward(){
+    console.log(this, 'forward')
     // return if we've already reached end of the slides
     if(this.activeImgIndex === this.sliderItems.length - 1) return
 
@@ -103,6 +117,7 @@ class Sliderize {
   }
 
   goBackward(){
+    console.log(this, 'backward')
     if(this.activeImgIndex === 0) return
 
     this.offset += this.slideLength
@@ -123,6 +138,6 @@ class Sliderize {
 
 }
 
-//Get slider container
+// Initialize slider
 const sliderDiv = document.getElementsByClassName('custom-product-slider')[0] 
-const slider = new Sliderize(sliderDiv, 86, 170, 370)
+const slider = new Sliderize(sliderDiv, 86, 170, 404)
